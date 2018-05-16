3511790ddf3347c3c227a7e258d0a058dd814fda
104969 <104969@SALA409.proway.treina>
2018-05-15 21:58:05 -0300

    Atualizado o exemplo 15

--- src/exemplo01/Formulario.java
+++ src/exemplo01/Formulario.java
@@ -2,6 +2,7 @@ package exemplo01;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Formulario {
 
@@ -9,28 +10,40 @@ public class Formulario {
 	public Formulario() {
 		
 		//JFrame
-		JFrame frm = new JFrame("Exercicio 01");
-		frm.setVisible(true);
-		frm.setSize(500, 300);
-		frm.setLocationRelativeTo(null);
-		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frm.setLayout(null);
+		JFrame frm = new JFrame("Exercicio 01"); //Criar o JFrame e colocar o nome do titulo
+		frm.setSize(500, 300); //Setar o tamanho da caixa
+		frm.setLocationRelativeTo(null); //Centralizar a caixa
+		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Quando o cliente fechar, a caixa fecha e não fica oculta
+		frm.setLayout(null); //Poder criar o layout dos elementos "Basico"
 		
 		//JLabel
-		JLabel lblNomeProduto = new JLabel("Informe o prodto");
-		lblNomeProduto.setBounds(20, 20, 150, 20);
+		JLabel lblNomeProduto = new JLabel("Informe o produto"); //Descrição
+		lblNomeProduto.setBounds(10, 20, 150, 20); //Posição da descrição
 		
-		JLabel lblPagamentoProduto = new JLabel("Informe a forma de pagamento");
-		lblPagamentoProduto.setBounds(20, 120, 180, 20);
+		JLabel lblPagamentoProduto = new JLabel("Informe a forma de pagamento"); //Descrição
+		lblPagamentoProduto.setBounds(10, 100, 180, 20); //Posição da descrição
 		
-		JLabel lblValorProduto = new JLabel("Informe o valor do produto");
-		lblValorProduto.setBounds(20, 200, 150, 20);
+		JLabel lblValorProduto = new JLabel("Informe o valor do produto"); //Descrição
+		lblValorProduto.setBounds(10, 180, 150, 20); //Posição da descrição
+		
+		//JTextField
+		JTextField txtNomeProduto = new JTextField(); //Criado a caixa para o cliente digitar
+		txtNomeProduto.setBounds(130, 20, 200, 20); //Posição da caixa
+		
+		JTextField txtValorProduto = new JTextField(); //Criado a caixa para o cliente digitar
+		txtValorProduto.setBounds(190, 100, 200, 20); //Posição da caixa
+		
 		
 		//Adiciionar componentes ao JFrame
 		frm.add(lblNomeProduto);
 		frm.add(lblPagamentoProduto);
 		frm.add(lblValorProduto);
 		
+		frm.add(txtNomeProduto);
+		frm.add(txtValorProduto);
+		
+		//Exibir o formulario e seus componentes
+		frm.setVisible(true); //Fazer com que a caixa fique visivel
 	}
 
 }
